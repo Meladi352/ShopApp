432df3f3ba1c4c14a60964e3996cdf4e1787b90c
Kseniia Surmenkova <surmenkovakseniia@gmail.com>
2023-11-03 12:12:16 +0200

    Init the project first page "Home" with tailwind

---
+++ shop-app/src/components/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+import styles from './Home.module.scss'
+
+const Home: FC = () => {
+    return <div className={styles.bg}>
+        Home
+        </div>
+
+}
+
+export default Home
---
+++ shop-app/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // other Tailwind CSS settings...
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // Add any other file paths that contain utility classes.
+  ],
+};
